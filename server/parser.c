5622dcb1cb39c9041ca0abc10786b65e70c70305
Jacob Brown <jsbrown@unc.edu>
2024-03-16 10:03:24 -0400

    Add function signatures for parsing

--- server/parser.c
+++ server/parser.c
@@ -1,6 +1,131 @@
+/*
+ * This program implements the function signatures defined in parser.h
+ * as well as any helper functions necessary. The intention for this 
+ * program is to parse the context free grammar as defined in RFC 822
+ *
+ * Author(s): Jacob Brown
+ *
+ * Date created: 3/15/2024
+*/
+
 #include "parser.h"
 
-int tester(void) {
-	return SyntaxError;
+int whitespace(char* input) 
+{
+	return TRUE;
 }
 
+int space(char* input)
+{
+	return TRUE;
+}
+
+int nullspace(char* input)
+{
+	return TRUE;
+}
+
+int reverse_path(char* input)
+{
+	return TRUE;
+}
+
+int path(char* input)
+{
+	return TRUE;
+}
+
+int mailbox(char* input)
+{
+	return TRUE;
+}
+
+int local_part(char* input)
+{
+	return TRUE;
+}
+
+int string(char* input)
+{
+	return TRUE;
+}
+
+int character(char* input)
+{
+	return TRUE;
+}
+
+int domain(char* input)
+{
+	return TRUE;
+}
+
+int element(char* input)
+{
+	return TRUE;
+}
+
+int name(char* input)
+{
+	return TRUE;
+}
+
+int letter(char* input)
+{
+	return TRUE;
+}
+
+int let_dig_str(char* input)
+{
+	return TRUE;
+}
+
+int let_dig(char* input)
+{
+	return TRUE;
+}
+
+int digit(char* input)
+{
+	return TRUE;
+}
+
+int newline(char* input)
+{
+	return TRUE;
+}
+
+int special(char* input)
+{
+	return TRUE;
+}
+
+int mail_from_non_terminal(char* input)
+{
+	return TRUE;
+}
+
+int rcpt_to_non_terminal(char* input)
+{
+	return TRUE;
+}
+
+int data_non_terminal(char* input)
+{
+	return TRUE;
+}
+
+int mail_from_cmd(char* input)
+{
+	return TRUE;
+}
+
+int rcpt_to_cmd(char* input)
+{
+	return TRUE;
+}
+
+int data_cmd(char* input)
+{
+	return TRUE;
+}
--- server/parser.h
+++ server/parser.h
@@ -1,4 +1,40 @@
-// Error code definitions for SMTP server.
-#define SyntaxError 0
+/* Definitions for error codes and top-level parsing functions 
+ * to be used by the SMTP server to process incoming TCP messages.
+ *
+ * Author(s): Jacob Brown
+ *
+ * Date created: 3/15/2024
+*/
 
-int tester(void);
+/* Return value definitions.
+ * ------------------------
+ *
+ * Return values other than 0 and 1 will be used to indicate error 
+ * messages as follows. These constants are defined here for clarity 
+ * throughout the parser.
+*/
+#define TRUE 0
+#define FALSE 1
+#define WHITESPACE_ERR 2
+#define PATH_ERR 3
+#define MAILBOX_ERR 4
+#define STRING_ERR 5
+#define ELEMENT_ERR 6
+#define NEWLINE_ERR 7
+#define MAIL_FROM_CMD_ERR 8
+#define RCPT_TO_CMD_ERR 9
+#define DATA_CMD_ERR 10
+
+/* High-level parsing non-terminals.
+ * --------------------------------
+ *
+ * Functions to parse major non-terminals from RFC 822 such as the 
+ * 'MAIL FROM', 'RCPT TO', and 'DATA' commands. 
+ *
+ * The input parameter
+ * for each of these functions represents the input received from 
+ * a connected TCP socket (or the command line during testing).
+*/
+int mail_from_cmd(char* input);
+int rcpt_to_cmd(char* input);
+int data_cmd(char* input);
